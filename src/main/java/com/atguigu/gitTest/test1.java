2daa85cf8e5e6f92770d502bd9e94d8429f9003d
ewq32198 <932377137@qq.com>
2021-01-15 20:45:36 +0800

    红烧牛肉面最好吃

---
+++ src/main/java/com/atguigu/gitTest/test1.java
@@ -0,0 +1,10 @@
+package com.atguigu.gitTest;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("红烧牛肉面最好吃");
+        System.out.println("红烧牛肉面最好吃");
+        System.out.println("红烧牛肉面最好吃");
+        System.out.println("红烧牛肉面最好吃");
+    }
+}
